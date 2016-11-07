3c76306fc9393243a0427c8c8da32c84102215a5
Francesco Montefoschi <francesco.monte@gmail.com>
2016-11-07 17:49:48 +0100

    Renamed mqx_upload_on_m4SoloX to udooneo-m4uploader

--- Android.mk
+++ Android.mk
@@ -4,6 +4,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_SRC_FILES:= mqx_upload_on_m4SoloX.c
-LOCAL_MODULE := mqx_upload_on_m4SoloX
+LOCAL_MODULE := udooneo-m4uploader
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
